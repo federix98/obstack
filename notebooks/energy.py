6ad5fd4ce9b4300c3936a912172b559d7786a212
federix98 <fededm43@gmail.com>
2022-06-02 17:01:41 +0200

    Added kind filter and scolling management

---
+++ notebooks/energy.py
@@ -0,0 +1,13 @@
+import pyRAPL
+
+pyRAPL.setup()
+
+@pyRAPL.measureit
+def foo():
+	i = 0
+	while(i<100000):
+		print(i)
+		i = i + 1
+
+foo()
+
--- notebooks/env/bin/activate.fish
+++ notebooks/env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/dimenna/AVV/git/notebooks/env"
+set -gx VIRTUAL_ENV "/home/dimenna/AVV/git/obstack/notebooks/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
